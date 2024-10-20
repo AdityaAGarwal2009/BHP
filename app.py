c162ae4132abd22ab4da1cf743ee4f36de4b9539
Aditya Agarwal <aditya6226agarwal@gmail.com>
2024-10-20 15:23:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,51 @@
-
+import json
+import pickle
+import numpy as np
 from flask import Flask, request, jsonify
-import util
 
 app = Flask(__name__)
 
-@app.route('/get_location_names', methods=['GET'])
+__locations = None
+__data_columns = None
+__model = None
+
+def get_estimated_price(location, sqft, bhk, bath):
+    try:
+        loc_index = __data_columns.index(location.lower())
+    except:
+        loc_index = -1
+
+    x = np.zeros(len(__data_columns))
+    x[0] = sqft
+    x[1] = bath
+    x[2] = bhk
+    if loc_index >= 0:
+        x[loc_index] = 1
+
+    return round(__model.predict([x])[0], 2)
+
 def get_location_names():
+    return __locations
+
+def load_saved_artifacts():
+    print("loading saved artifacts...start")
+    global __data_columns
+    global __locations
+
+    with open("columns.json", "r") as f:
+        __data_columns = json.load(f)['data_columns']
+        __locations = __data_columns[3:]
+
+    global __model
+    with open("banglore_home_prices_model.pickle", "rb") as f:
+        __model = pickle.load(f)
+
+    print("loading saved artifacts...done")
+
+@app.route('/get_location_names', methods=['GET'])
+def get_location_names_endpoint():
     response = jsonify({
-        'locations': util.get_location_names()
+        'locations': get_location_names()
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
@@ -20,13 +58,12 @@ def predict_home_price():
     bath = int(request.form['bath'])
 
     response = jsonify({
-        'estimated_price': util.get_estimated_price(location, total_sqft, bhk, bath)
+        'estimated_price': get_estimated_price(location, total_sqft, bhk, bath)
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
 if __name__ == "__main__":
     print("Starting Python Flask Server For Home Price Prediction...")
-    util.load_saved_artifacts()
+    load_saved_artifacts()
     app.run()
-
