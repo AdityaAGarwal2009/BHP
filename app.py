b812258c02d10f76a4ec8c71c9db2cce155fbf63
Aditya Agarwal <aditya6226agarwal@gmail.com>
2024-10-20 21:38:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,11 @@ app = Flask(__name__)
 
 # Load the model
 __model = None
-__data_columns = [["total_sqft", "bath", "bhk", "1st block jayanagar", "1st phase jp nagar", "2nd phase judicial layout", "2nd stage nagarbhavi", "5th block hbr layout", "5th phase jp nagar", "6th phase jp nagar", "7th phase jp nagar", "8th phase jp nagar", "9th phase jp nagar", "aecs layout", "abbigere", "akshaya nagar", "ambalipura", "ambedkar nagar", "amruthahalli", "anandapura", "ananth nagar", "anekal", "anjanapura", "ardendale", "arekere", "attibele", "beml layout", "btm 2nd stage", "btm layout", "babusapalaya", "badavala nagar", "balagere", "banashankari", "banashankari stage ii", "banashankari stage iii", "banashankari stage v", "banashankari stage vi", "banaswadi", "banjara layout", "bannerghatta", "bannerghatta road", "basavangudi", "basaveshwara nagar", "battarahalli", "begur", "begur road", "bellandur", "benson town", "bharathi nagar", "bhoganhalli", "billekahalli", "binny pete", "bisuvanahalli", "bommanahalli", "bommasandra", "bommasandra industrial area", "bommenahalli", "brookefield", "budigere", "cv raman nagar", "chamrajpet", "chandapura", "channasandra", "chikka tirupathi", "chikkabanavar", "chikkalasandra", "choodasandra", "cooke town", "cox town", "cunningham road", "dasanapura", "dasarahalli", "devanahalli", "devarachikkanahalli", "dodda nekkundi", "doddaballapur", "doddakallasandra", "doddathoguru", "domlur", "dommasandra", "epip zone", "electronic city", "electronic city phase ii", "electronics city phase 1", "frazer town", "gm palaya", "garudachar palya", "giri nagar", "gollarapalya hosahalli", "gottigere", "green glen layout", "gubbalala", "gunjur", "hal 2nd stage", "hbr layout", "hrbr layout", "hsr layout", "haralur road", "harlur", "hebbal", "hebbal kempapura", "hegde nagar", "hennur", "hennur road", "hoodi", "horamavu agara", "horamavu banaswadi", "hormavu", "hosa road", "hosakerehalli", "hoskote", "hosur road", "hulimavu", "isro layout", "itpl", "iblur village", "indira nagar", "jp nagar", "jakkur", "jalahalli", "jalahalli east", "jigani", "judicial layout", "kr puram", "kadubeesanahalli", "kadugodi", "kaggadasapura", "kaggalipura", "kaikondrahalli", "kalena agrahara", "kalyan nagar", "kambipura", "kammanahalli", "kammasandra", "kanakapura", "kanakpura road", "kannamangala", "karuna nagar", "kasavanhalli", "kasturi nagar", "kathriguppe", "kaval byrasandra", "kenchenahalli", "kengeri", "kengeri satellite town", "kereguddadahalli", "kodichikkanahalli", "kodigehaali", "kodigehalli", "kodihalli", "kogilu", "konanakunte", "koramangala", "kothannur", "kothanur", "kudlu", "kudlu gate", "kumaraswami layout", "kundalahalli", "lb shastri nagar", "laggere", "lakshminarayana pura", "lingadheeranahalli", "magadi road", "mahadevpura", "mahalakshmi layout", "mallasandra", "malleshpalya", "malleshwaram", "marathahalli", "margondanahalli", "marsur", "mico layout", "munnekollal", "murugeshpalya", "mysore road", "ngr layout", "nri layout", "nagarbhavi", "nagasandra", "nagavara", "nagavarapalya", "narayanapura", "neeladri nagar", "nehru nagar", "ombr layout", "old airport road", "old madras road", "padmanabhanagar", "pai layout", "panathur", "parappana agrahara", "pattandur agrahara", "poorna pragna layout", "prithvi layout", "r.t. nagar", "rachenahalli", "raja rajeshwari nagar", "rajaji nagar", "rajiv nagar", "ramagondanahalli", "ramamurthy nagar", "rayasandra", "sahakara nagar", "sanjay nagar", "sarakki nagar", "sarjapur", "sarjapur  road", "sarjapura - attibele road", "sector 2 hsr layout", "sector 7 hsr layout", "seegehalli", "shampura", "shivaji nagar", "singasandra", "somasundara palya", "sompura", "sonnenahalli", "subramanyapura", "sultan palaya", "tc palaya", "talaghattapura", "thanisandra", "thigalarapalya", "thubarahalli", "thyagaraja nagar", "tindlu", "tumkur road", "ulsoor", "uttarahalli", "varthur", "varthur road", "vasanthapura", "vidyaranyapura", "vijayanagar", "vishveshwarya layout", "vishwapriya layout", "vittasandra", "whitefield", "yelachenahalli", "yelahanka", "yelahanka new town", "yelenahalli", "yeshwanthpur"]]  # Your list of data columns
+__data_columns = ["total_sqft", "bath", "bhk"] + [
+    "1st block jayanagar", "1st phase jp nagar", "2nd phase judicial layout", 
+    # ... (add all other location names here)
+    "yeshwanthpur"
+]  # Your list of data columns, including location names
 
 def load_saved_artifacts():
     global __model
@@ -20,11 +24,8 @@ def get_estimated_price(location, sqft, bhk, bath):
     try:
         loc_index = __data_columns.index(location.lower())
     except ValueError:
-        print(f"Invalid location: {location}. Valid locations are: {__data_columns[3:]}")  # Add this line
+        print(f"Invalid location: {location}. Valid locations are: {__data_columns[3:]}")
         loc_index = -1
-
-    # Rest of the code...
-
 
     x = np.zeros(len(__data_columns))
     x[__data_columns.index("total_sqft")] = sqft
@@ -36,9 +37,22 @@ def get_estimated_price(location, sqft, bhk, bath):
     predicted_price = __model.predict([x])[0]
     return round(predicted_price, 2)
 
+def get_location_names():
+    return __data_columns[3:]  # This returns all location names
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/get_location_names', methods=['GET'])
+def get_location_names_endpoint():
+    try:
+        locations = get_location_names()
+        print(f"Locations fetched: {locations}")  # Log fetched locations
+        return jsonify({'locations': locations})
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({"error": str(e)}), 500
 
 @app.route('/predict_home_price', methods=['POST'])
 def predict_home_price():
@@ -49,17 +63,15 @@ def predict_home_price():
         bhk = int(data['bhk'])
         bath = int(data['bath'])
 
-        print(f"Received data: sqft={total_sqft}, location={location}, bhk={bhk}, bath={bath}")  # Add this line
+        print(f"Received data: sqft={total_sqft}, location={location}, bhk={bhk}, bath={bath}")
 
         estimated_price = get_estimated_price(location, total_sqft, bhk, bath)
 
         return jsonify({'estimated_price': estimated_price})
     except Exception as e:
-        print(f"Error: {e}")  # Add this line to log the error
+        print(f"Error: {e}")
         return jsonify({"error": str(e)}), 500
-
 
 if __name__ == "__main__":
     load_saved_artifacts()
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
-
